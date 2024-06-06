540c98aeade4101d65c814b0b206982cfeb06bfa
stonelly <stonelly_chong@yahoo.com>
2024-06-07 04:07:28 +0800

    update notebook 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ with st.form('predict'):
     submit = st.form_submit_button('Predict')
 
 if submit:
-    RegionName = label_encoder.transform([RegionName])[1]
+    #RegionName = label_encoder.transform([RegionName])[1]
     Generation = generation_bins[Generation]
     Sex = sex_bins[Sex]
     input_data = [[Sex, Generation, Population, GDP, GrossNationalIncome, InflationRate, EmploymentPopulationRatio]]
